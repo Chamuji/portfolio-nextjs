5a82f8f8b67f8e9df40ff62d837b2b5d7d20ce00
chamuji <s1412052@s.do-johodai.ac.jp>
2022-10-22 21:52:22 +0900

    Header

---
+++ pages/components/Header.jsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import style from "../../styles/header.module.css";
+
+const Header = () => {
+  return (
+    <header className={style.container}>
+      <nav className={style.gnav}>
+        <ul className={style.gnavlist}>
+          <li className={style.gnavitem}>
+            <Link href="/">
+              <a>Top</a>
+            </Link>
+          </li>
+          <li className={style.gnavitem}>
+            <Link href="/about">
+              <a>ABOUT</a>
+            </Link>
+          </li>
+          <li className={style.gnavitem}>
+            <Link href="/portfolio">
+              <a>PORTFOLIO</a>
+            </Link>
+          </li>
+          <li className={style.gnavitem}>
+            <Link href="/skill">
+              <a>SKILL</a>
+            </Link>
+          </li>
+          <li className={style.gnavitem}>
+            <Link href="/blog">
+              <a>BLOG</a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
